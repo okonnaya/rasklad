e052655567febddc3f280048ce526ed6433f7891
okonnaya <95911284+okonnaya@users.noreply.github.com>
2026-09-11 20:25:30 +0300

    Update catalog bundle for domain-independent navigation

--- docs/cards.js
+++ docs/cards.js
@@ -2265,9 +2265,6 @@ function O_Menubar_defineProperty(obj, key, value) { if (key in obj) { Object.de
 
 
 
-
-// const adressPart = ':8080/'
-var adressPart = '.adc.ac/';
 var menu = [{
   text: '',
   url: '/index.html'
@@ -2294,9 +2291,6 @@ var O_MenuBar = /*#__PURE__*/function (_React$Component) {
     var _this;
     O_Menubar_classCallCheck(this, O_MenuBar);
     _this = _super.call(this, props);
-    O_Menubar_defineProperty(O_Menubar_assertThisInitialized(_this), "getPathFromUrl", function (url) {
-      return url.split(adressPart)[0];
-    });
     O_Menubar_defineProperty(O_Menubar_assertThisInitialized(_this), "handleSearchInput", function (searchInputValue) {
       var isSearchButtonDisabled = true;
       if (searchInputValue.length >= 3) {
@@ -2310,8 +2304,7 @@ var O_MenuBar = /*#__PURE__*/function (_React$Component) {
     O_Menubar_defineProperty(O_Menubar_assertThisInitialized(_this), "handleSearchSubmit", function () {
       var searchInputValue = _this.state.searchInputValue;
       if (searchInputValue.length >= 3) {
-        var url = _this.getPathFromUrl(window.location.href);
-        window.location.href = url + adressPart + 'search.html?request=' + searchInputValue;
+        window.location.href = '/search.html?request=' + encodeURIComponent(searchInputValue);
       }
     });
     O_Menubar_defineProperty(O_Menubar_assertThisInitialized(_this), "handleFocus", function () {
@@ -2350,8 +2343,6 @@ var O_MenuBar = /*#__PURE__*/function (_React$Component) {
   //   })
   // }
 
-  // вынести функцию в утилиты
-
   // ExtendedSearchBar = () => {
   //   searchBox.classList.add('M_SearchBarActive')
   // }
@@ -2362,7 +2353,6 @@ var O_MenuBar = /*#__PURE__*/function (_React$Component) {
         searchInputValue = _this$state.searchInputValue,
         current = _this$state.current,
         isToggled = _this$state.isToggled;
-      var url = this.getPathFromUrl(window.location.href);
       var menuElementsclasses = classnames_default()({
         'M_MenuElements': true,
         'Shows': isToggled
@@ -2372,7 +2362,7 @@ var O_MenuBar = /*#__PURE__*/function (_React$Component) {
       }, /*#__PURE__*/react.createElement(A_MenuMobileToggle, {
         onClick: this.toggleClass
       }), /*#__PURE__*/react.createElement(A_MenuLogo, {
-        url: url + adressPart
+        url: "/"
       }), /*#__PURE__*/react.createElement("div", {
         className: menuElementsclasses
       }, /*#__PURE__*/react.createElement(A_MenuElement, {
